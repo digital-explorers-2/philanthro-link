78f0b47e0b614b4611d766ed18b3060d8ceda711
khalifa47 <khalifafumo5@gmail.com>
2024-05-11 13:18:27 +0300

    chore: clean up boilerplate
    Remove supabase default code. Project init ready for fresh start. `globals.css` to be reset automatically on install shadcn theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "PhilanthroLink",
+  description:
+    "The one-stop crowdfunding platform dedicated to connecting donors with causes they care about",
 };
 
 export default function RootLayout({
