6c07a01bf21f1339067eb31c4452035ad0e582cb
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-15 12:50:17 +0300

    feat(about): add about text

---
+++ app/About/page.tsx
@@ -0,0 +1,11 @@
+
+export default function AboutPage() {
+  return (
+    <div>
+        <div>
+        <h1>About</h1>
+      <p>PhilanthroLink is a crowdfunding platform dedicated to connecting donors with causes they care about. It provides a seamless way for individuals and organizations to donate to verified charitable projects while fostering a sense of community and collaboration in the realm of philanthropy.</p>
+        </div>
+    </div>
+  );
+}
