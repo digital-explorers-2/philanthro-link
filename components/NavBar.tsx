d19f19c2fa8311cc395239baa7cd9d4e75ddd502
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-05-16 22:32:32 +0300

    feat: add donation header and search bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     // Header section with flex layout and styling
     <header className="absolute w-full z-10">
-      <div className="top-0 flex h-16 items-center gap-4 bg-transparent px-4 md:px-6">
+      <div className="top-0 flex h-16 items-center gap-4 bg-transparent px-4 md:px-6 mt-2">
       {/* Navigation menu with flex layout */}
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         {/* Link to home page with a PlaneTakeoff icon */}
