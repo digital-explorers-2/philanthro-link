9ffe10a7ab5f29dcc4e561daba2855253810298d
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-05-16 22:45:56 +0300

    style: modify position and style of donation elements

--- components/DonationSection.tsx
+++ components/DonationSection.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Button } from "@/components/ui/button"; // Importing Button component from custom UI library
-import { Search } from 'lucide-react'; // Importing the Search icon from Lucide
+import { Button } from "@/components/ui/button";
+import { Search } from 'lucide-react'; 
 
 function DonationSection() {
   return (
-    <div className="p-6 mt-16">
-      <h2 className="text-3xl font-bold text-center font-dm-sans mb-10">
-        <span className="text-black">Open</span> 
-        <span className="text-primary"> donations</span>
-      </h2>
-      <div className="flex justify-center"> {/* Centering the search bar */}
-        <div className="w-[800px] h-[55px] flex items-center border border-gray-300 rounded-full px-4 py-2">
-          <input
-            type="text"
-            placeholder="Find donations..."
-            className="flex-grow outline-none"
-          />
-          <Button size="icon" className="bg-transparent">
-              <Search className="w-6 h-6 text-gray-500" /> {/* Lucide magnifying glass icon */}
-          </Button>
+    <div className="p-6 bg-gray-100">
+      <div className="mt-16"> 
+        <h2 className="text-3xl font-bold text-center font-dm-sans mb-10">
+          <span className="text-black">Open</span> 
+          <span className="text-primary"> donations</span>
+        </h2>
+        <div className="flex justify-center"> 
+          <div className="w-[800px] h-[55px] flex items-center border border-gray-300 rounded-full px-4 py-2 shadow-lg">
+            <input
+              type="text"
+              placeholder="Find donations..."
+              className="flex-grow outline-none"
+            />
+            <Button size="icon" className="bg-transparent">
+                <Search className="w-6 h-6 text-gray-500" /> 
+            </Button>
+          </div>
         </div>
       </div>
     </div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,56 +1,47 @@
-import Link from "next/link"; // Importing Link component from Next.js
-import { Menu, Phone } from "lucide-react"; // Importing Menu and PlaneTakeoff icons from lucide-react
-import { Button } from "@/components/ui/button"; // Importing Button component from custom UI library
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"; // Importing Sheet components from custom UI library
+import Link from "next/link"; 
+import { Menu, Phone } from "lucide-react"; 
+import { Button } from "@/components/ui/button"; 
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"; 
 
 export default function NavBar() {
   return (
-    // Header section with flex layout and styling
     <header className="absolute w-full z-10">
       <div className="top-0 flex h-16 items-center gap-4 bg-transparent px-4 md:px-6 mt-2">
-      {/* Navigation menu with flex layout */}
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        {/* Link to home page with a PlaneTakeoff icon */}
         <Link href="#" className="flex items-center gap-2 text-sm font-semibold md:text-base">
           <img src="/logo.png" alt="Logo" className="h-7 w-21" />
-          <span className="sr-only">PhilanthroLink</span> {/* Screen reader only text for accessibility */}
+          <span className="sr-only">PhilanthroLink</span>
         </Link>
-        <div className="flex-grow"></div> {/* Placeholder to push links to the right */}
+        <div className="flex-grow"></div>
       </nav>
       
-      {/* Links container */}
-      <div className="hidden md:flex items-center space-x-4 mr-auto text-sm"> {/* Container for text links with ml-auto to align them to the right */}
-        {/* Individual links for different sections */}
+      <div className="hidden md:flex items-center space-x-4 mr-auto text-sm">
         <Link href="#" className="text-muted-foreground transition-colors hover:text-primary text-white">Home</Link>
         <Link href="#" className="text-muted-foreground transition-colors hover:text-primary text-white">About</Link>
         <Link href="#" className="text-muted-foreground transition-colors hover:text-primary text-white">Contact</Link>
         <Link href="#" className="text-muted-foreground transition-colors hover:text-primary text-white">Help</Link>
       </div>
 
-      <div className="hidden md:flex items-center space-x-4 ml-auto text-sm"> {/* Container for text links with ml-auto to align them to the right */}
+      <div className="hidden md:flex items-center space-x-4 ml-auto text-sm">
         <Link href="#" className="flex items-center gap-2 text-sm font-semibold">
-            <Phone className="h-4 w-4 text-white hover:text-primary" /> {/* Phone icon */}
+            <Phone className="h-4 w-4 text-white hover:text-primary" />
         </Link>
         <Link href="#" className="text-muted-foreground transition-colors hover:text-primary text-white">+372 5882 5555</Link>
       </div>
       
-      {/* Mobile navigation menu */}
       <Sheet>
         <SheetTrigger asChild>
           <Button size="icon" className="shrink-0 md:hidden bg-transparent">
             <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span> {/* Screen reader only text for accessibility */}
+            <span className="sr-only">Toggle navigation menu</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="left">
-          {/* Navigation menu for mobile view */}
           <nav className="grid gap-6 text-sm font-medium">
-            {/* Link to home page with a PlaneTakeoff icon */}
             <Link href="#" className="flex items-center gap-2 text-sm font-semibold">
             <img src="/logo.png" alt="Logo" className="h-7 w-21" />
-              <span className="sr-only">PhilanthroLink</span> {/* Screen reader only text for accessibility */}
+              <span className="sr-only">PhilanthroLink</span>
             </Link>
-            {/* Individual links for different sections */}
             <Link href="#" className="text-muted-foreground hover:text-foreground">Home</Link>
             <Link href="#" className="text-muted-foreground hover:text-foreground">About</Link>
             <Link href="#" className="text-muted-foreground hover:text-foreground">Contact</Link>
@@ -58,12 +49,12 @@ export default function NavBar() {
           </nav>
         </SheetContent>
       </Sheet>
-      <div className="md:hidden items-center space-x-4 ml-auto text-sm"> {/* Container for text links with ml-auto to align them to the right */}
+      <div className="md:hidden items-center space-x-4 ml-auto text-sm">
       <Button size="icon" className="shrink-0 md:hidden bg-transparent">
         <Link href="#" className="flex items-center gap-2 text-sm font-semibold">
             <Phone href="#" className="h-5 w-5" />
         </Link>
-        <span className="sr-only">Contact us</span> {/* Screen reader only text for accessibility */}
+        <span className="sr-only">Contact us</span>
       </Button>
       </div>
       </div>
