6c325f4de1e3790cca8bb59411bcaeb4847365ac
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-19 19:28:54 +0300

    feat: create a form using react-hook-form

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,13 +1,34 @@
 "use client"
 
 import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(8, { message: "Must be more than 8 characters" }),
   confirmPassword: z.string().min(8, { message: "Must be more than 8 characters" }),
 })
+
 .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirm"],
   });
+export function Registration() {
+    // 1. Define the form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  })
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(values)
+  }
+
+  }
+
