010d6950fb84c9e523b4775fbacc54fe6f255526
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-20 18:11:00 +0300

    feat: add headings to contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return( 
+    <div className="md:grid grid-cols-2 mx-auto md:h-screen m-10">
+      <div className="flex flex-col justify-start items-start p-2">
+        <h1 className="font-bold text-6xl mb-10">Contact us</h1>
+        <h3 className="font-normal text-2xl mb-10">Feel free to reach out</h3>
+
+    </div>
+    </div>
+    );
+}
