dece0b963fbd43c2edbc6acb6fdefeda8b071474
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-20 10:09:40 +0300

    feat: definition of the form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,28 @@
 "use client"
 
 import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(8, { message: "Must be more than 8 characters" }),
 })
+
+export default function Login(){
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+}
