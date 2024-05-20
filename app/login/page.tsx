a89cd9beac870fbb9e48f57c5fd1b50795f2a742
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-20 15:09:29 +0300

    style: format the style of the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,10 +7,8 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
@@ -38,10 +36,13 @@ export default function Login(){
   }
 
   return (
-    <> 
-    <h1 className="text-xl font-bold mt-4 mb-3">Create an account</h1>
-    <p className="text-base mb-3">Enter your email to sign up for this app.<br/>
-    If you have an existing account, <a href="#" className="font-bold">sign in</a></p>
+    // 
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="p-6 w-full max-w-md">
+
+    <h1 className="text-xl text-center font-bold mt-4 mb-3">Sign in to your account</h1>
+    <p className="text-base text-center mb-3">Sign in to your account.<br/>
+    If you are new to Philathrolink, <a href="#" className="font-bold">sign up</a></p>
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
@@ -69,7 +70,7 @@ export default function Login(){
           )}
         />
 <div className="flex justify-center">
-    <Button variant="outline" type="submit" className="mb-1 bg-black text-white">Sign up with email</Button>
+    <Button variant="outline" type="submit" className="mb-1 bg-black text-white w-full">Sign in</Button>
   </div>      
   <div className="flex items-center mb-4">
       <div className="flex-grow h-px bg-gray-300" />
@@ -77,16 +78,17 @@ export default function Login(){
       <div className="flex-grow h-px bg-gray-300" /> 
        </div>
        <div className="flex justify-center">
-      <Button variant="outline" className="flex items-center space-x-2 my-1" type="button"><svg xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+      <Button variant="outline" className="flex items-center space-x-2 my-1 w-full" type="button"><svg xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
         <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
         </svg><span>Google</span></Button>
         </div>
-      <p className="text-sm">By clicking continue, you agree to our <a href="#" className="font-bold">Terms of<br/>
+      <p className="text-sm text-center">By clicking continue, you agree to our <a href="#" className="font-bold">Terms of<br/>
        Service</a> and <a href="#" >Privacy Policy</a></p>
   </form>
       
     </Form>
-    </>
+    </div>
+    </div>
   )
 }
 
