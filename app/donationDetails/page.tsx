3edac23ef776ebdfdc9f63aa61c09c4e407610b2
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-05-27 10:34:30 +0300

    feat: add donation cards

--- app/donationDetails/page.tsx
+++ app/donationDetails/page.tsx
@@ -6,6 +6,8 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
 
 export default function DonationDetailsPage() {
 return(
@@ -37,7 +39,53 @@ return(
                 <p>Floods in Kenya are a recurring natural disaster, often caused by heavy rainfall, overflowing rivers, or inadequate drainage systems, especially in urban areas. Kenya typically experiences two rainy seasons: the long rains from March to May and the short rains from October to December. </p>
                 <p>During these periods, flooding can occur, leading to displacement of people, destruction of property, loss of lives, and disruption of infrastructure and livelihoods. </p>
             </div>
+            </div>
+            <div>
+            <Card>
+            <CardHeader>
+                <CardTitle>Sh. 2000 <h3>of Ksh. 45000</h3></CardTitle>
+                <CardDescription>100 donations <pre>Ksh.25,000 to go</pre></CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline" type="button" className="mb-1 bg-black text-white w-full">Donate now</Button>
+            </CardFooter>
+            </Card>
 
+                            {/* These are the cards for other values of donation */}
+                            <Card>
+            <CardHeader>
+                <CardTitle>15K <h3>KSH</h3></CardTitle>
+                <CardDescription>emergency food, supplies and long-term aid</CardDescription>
+            </CardHeader>
+            </Card>
+
+            <Card>
+            <CardHeader>
+                <CardTitle>50K <h3>KSH</h3></CardTitle>
+                <CardDescription>emergency food, supplies and long-term aid</CardDescription>
+            </CardHeader>
+            </Card>
+
+            <Card>
+            <CardHeader>
+                <CardTitle>75K <h3>KSH</h3></CardTitle>
+                <CardDescription>emergency food, supplies and long-term aid</CardDescription>
+            </CardHeader>
+            </Card>
+
+            <Card>
+            <CardHeader>
+                <CardTitle>100K <h3>KSH</h3></CardTitle>
+                <CardDescription>emergency food, supplies and long-term aid</CardDescription>
+            </CardHeader>
+            </Card>
+
+            <Card>
+            <CardHeader>
+                <CardTitle>500K <h3>KSH</h3></CardTitle>
+                <CardDescription>emergency food, supplies and long-term aid</CardDescription>
+            </CardHeader>
+            </Card>
             </div>
 
 
