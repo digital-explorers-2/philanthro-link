87976218e347e3c1530f8eb083496fc513e3f95f
khalifa47 <khalifafumo5@gmail.com>
2024-05-27 20:43:20 +0300

    feat: perform pagination
    TODO: make pagination smooth and safe to prevent unsafe navigation.
    
    Closes #64

--- app/api/donations/route.ts
+++ app/api/donations/route.ts
@@ -1,13 +1,19 @@
 import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  let page = searchParams.get("page") || 1;
+  page = parseInt(page as string, 10);
+
+  const limit = 6;
   const supabase = createClient();
 
   const { data: donations, error } = await supabase
     .from("donations")
     .select(`*, descriptions(*)`)
-    .order("created_at", { ascending: false });
+    .order("created_at", { ascending: false })
+    .range((page - 1) * limit, page * limit - 1);
 
   if (error) {
     return new NextResponse(JSON.stringify({ error }), {
@@ -15,7 +21,11 @@ export async function GET() {
     });
   }
 
-  return new NextResponse(JSON.stringify(donations), {
+  const { count } = await supabase
+    .from("donations")
+    .select("id", { count: "exact" });
+
+  return new NextResponse(JSON.stringify({ donations, count }), {
     status: 200,
   });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,18 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import React from "react";
 
-const getDonations = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/donations`, {
-    next: { revalidate: 0 },
-  });
+export type DonationsObject = {
+  donations: Donation[];
+  count: number;
+};
+
+const getDonations = async (page: number) => {
+  const res = await fetch(
+    `${process.env.BASE_URL}/api/donations?page=${page}`,
+    {
+      next: { revalidate: 0 },
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -28,8 +36,15 @@ const getCategories = async () => {
   return res.json();
 };
 
-export default async function page() {
-  const donationsPromise: Promise<Donation[]> = getDonations();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const page = searchParams.page
+    ? parseInt(searchParams.page as string, 10)
+    : 1;
+  const donationsPromise: Promise<DonationsObject> = getDonations(page);
   const categoriesPromise: Promise<Category[]> = getCategories();
 
   const [donations, categories] = await Promise.all([
