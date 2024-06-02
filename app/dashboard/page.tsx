87c84f47852f2c1052733e899f8b9a8179f0feb8
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-06-02 23:31:30 +0300

    feat: add the form component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import Link from 'next/link';
 import { Home, Plus, List, Edit, LogOut } from 'lucide-react';
@@ -6,6 +7,17 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
  
 const formSchema = z.object({
   main_title: z.string(),
@@ -177,8 +189,129 @@ function DashboardPage() {
             <TabsContent value="add-donations">
             <h1 className="text-2xl font-semibold mb-4">Add Donations</h1>
               <p className="text-gray-500 mb-6">Create Donation</p>
-
-
+              <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+        control={form.control}
+        name="main_title"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Main Title</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter main title" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="subtitle"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Subtitle</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter subtitle" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="category"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Category</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter category" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="image"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Add an image</FormLabel>
+            <FormControl>
+              <Input type="file" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="challenge"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Challenge</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter challenge being faced" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="solution"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Solution</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter solution to the challenge" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="usage"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>How donation will be used</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter how donation will be used" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="amount"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Target amount</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter target amount(goal)" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="currency"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Currency</FormLabel>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <Button variant="outline"
+                type="submit"
+                className="mb-1 bg-black text-white w-full"
+                  >Submit
+                </Button>
+      </form>
+    </Form>
 
             </TabsContent>
 
