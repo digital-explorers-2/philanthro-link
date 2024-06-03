87059fb8795603187647fd7f6c3a1b8d8fc15d7a
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-06-03 23:22:38 +0300

    feat: create the form schema

--- app/dashboard/edit-profile/page.tsx
+++ app/dashboard/edit-profile/page.tsx
@@ -16,9 +16,37 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const formSchema = z
+  .object({
+    username: z.string(),
+    email: z.string().email({ message: "Invalid email address" }),
+    changePassword: z
+      .string()
+      .min(8, { message: "Must be more than 8 characters" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Must be more than 8 characters" }),
+  })
+
+  .refine((data) => data.changePassword === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
 
 export default function ProfilePage() {
-  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      changePassword: "",
+      confirmPassword: "",
+    },
+  });
+  // 2. Define a submit handler.
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
   return (
    
   );
