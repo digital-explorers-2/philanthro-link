f0da7906b7da5fc8859c3c8cf50c49e4c6213aa5
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-06-03 12:24:22 +0300

    feat: import radio group

--- app/donationDetails/page.tsx
+++ app/donationDetails/page.tsx
@@ -18,6 +18,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 const donateMoney = [
   {
