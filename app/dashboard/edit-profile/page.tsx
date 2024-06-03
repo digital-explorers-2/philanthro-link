99b1a5e5dace3e67f1bf2b197158d009295e0981
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-06-03 23:20:25 +0300

    feat: add necessary imports

---
+++ app/dashboard/edit-profile/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import SideBar from "@/components/SideBar";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+
+export default function ProfilePage() {
+  
+  return (
+   
+  );
+}
