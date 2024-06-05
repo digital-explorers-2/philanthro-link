bb1868ef420ab0ecf6f1a294898da305172ed668
khalifa47 <khalifafumo5@gmail.com>
2024-06-05 11:21:22 +0300

    feat: add and cleanup links, fix header display

--- components/ClientRootLayout.tsx
+++ components/ClientRootLayout.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-import React from 'react';
-import NavBar from '@/components/NavBar';
-import { usePathname } from 'next/navigation';
+import React from "react";
+import NavBar from "@/components/NavBar";
+import { usePathname } from "next/navigation";
 
 const ClientRootLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   // Check if the current path is the dashboard
-  const isDashboard = pathname?.startsWith('/dashboard');
+  const isDashboard = pathname?.startsWith("/dashboard");
+  const isHomePage = pathname === "/";
 
   return (
     <main className="min-h-screen flex flex-col">
-      {!isDashboard && <NavBar />}
+      {!isDashboard && <NavBar isHomePage={isHomePage} />}
       {children}
     </main>
   );
