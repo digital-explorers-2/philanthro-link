b20ebcfdd4d2529ff5ab5224c50ccf99e8b99a44
khalifa47 <khalifafumo5@gmail.com>
2024-06-10 10:45:40 +0300

    test: update landing snapshot and create about page snapshot

--- app/login/page.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "./page";
-
-it("Login Page: Displays 'Sign in' text", () => {
-  render(<Login />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Sign in");
-});
