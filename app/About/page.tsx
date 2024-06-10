5a73a09c38ded1abcad917c409ca70aff2cad4f1
khalifa47 <khalifafumo5@gmail.com>
2024-06-10 12:13:12 +0300

    style: fix margins on about

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -3,19 +3,19 @@ import FooterSecondary from "@/components/FooterSecondary";
 export default function AboutPage() {
   return (
     <div className="md:mx-20 md:h-screen my-20">
-      <div className="flex flex-col justify-start items-start p-8 mr-10">
+      <div className="flex flex-col justify-start items-start p-8">
         <h1 className="font-bold text-6xl mb-10">About</h1>
         <h3 className="font-normal text-2xl mb-10 text-slate-400">Our Story</h3>
         <div className="flex flex-col md:flex-row gap-10 md:gap-20">
           <div className="w-full md:w-1/2">
-            <p className="mb-10 font-normal md:text-lg leading-7 justify-par">
+            <p className="mb-10 leading-7 justify-par">
               Welcome to PhilanthroLink, where generosity meets purpose. We are
               a dynamic crowdfunding platform dedicated to bridging the gap
               between passionate donors and the causes they care about the most.
               Our mission is to make philanthropy accessible, transparent, and
               impactful for everyone involved.
             </p>
-            <p className="mb-10 font-normal md:text-lg leading-7 justify-par">
+            <p className="mb-10 leading-7 justify-par">
               At PhilanthroLink, we believe in the power of community and
               collaboration. Our platform offers a seamless and secure way for
               individuals and organizations to support verified charitable
